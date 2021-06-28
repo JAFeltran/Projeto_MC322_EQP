01f14beb0b328f45d6773b7a9965116b0c03132f
João Augusto Rosa Feltran <j174083@dac.unicamp.br>
2021-06-28 07:32:04 -0300

    modifica construtor do heroi

--- src/src/Ator/IHeroi.java
+++ src/src/Ator/IHeroi.java
@@ -12,6 +12,8 @@ public interface IHeroi extends IAtor, ICombate, IAcao {
     public int getValorItemInventario(int posicao);
 
     // Outras funcoes
+    // Define os itens iniciais com base na fase
+    public void definirItensIniciais(int fase);
     // Verifica se o heroi carrega uma chave
     public boolean pegouChave(int fase);
     // Recupera uma porcentagem da vida maxima do heroi
